aa07a7c77357a2131f4b0c43d7aee0e034ea87f5
Isaac Noland <isaac.r.noland@gmail.com>
2014-04-29 13:06:55 -0700

    Reset Character

---
+++ Assets/Scripts/ResetCharacter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetCharacter : MonoBehaviour {
+
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.R)){
+			//Debug.Log ("Reset");
+			transform.position = new Vector3(0, 2, 0);
+		}
+	}
+}

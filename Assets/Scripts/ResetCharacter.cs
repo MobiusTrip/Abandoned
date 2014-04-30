cfdffb6c18823517cc6eb6e3f544628127aacb1c
Isaac Noland <isaac.r.noland@gmail.com>
2014-04-30 12:56:23 -0700

    ResetCharacter fix
    took out random code snippets found in script

--- Assets/Scripts/ResetCharacter.cs
+++ Assets/Scripts/ResetCharacter.cs
@@ -3,21 +3,9 @@ using System.Collections;
 
 public class ResetCharacter : MonoBehaviour {
 
-<<<<<<< HEAD
-	// Use this for initialization
-	void Start () { 
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyUp(KeyCode.R)){
-			//Debug.Log ("R");
-=======
 	void Update () {
 		if(Input.GetKeyUp(KeyCode.R)){
 			//Debug.Log ("Reset");
->>>>>>> 14845e6838f691e7d83e8827b731e1eb5dcd0a98
 			transform.position = new Vector3(0, 2, 0);
 		}
 	}
